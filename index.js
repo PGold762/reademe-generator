fa7f57e606debe6acde4032c58375b88d1ebb9ea
PGold <pglz762@gmail.com>
2023-08-27 16:06:55 -0500

    Completed the coding

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-// TODO: Include packages needed for this application
+// Const's to use mods and use of MarkDown.js page 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter project description:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter how to install this app:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter the contributions:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter the testing information:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github Username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like?',
+        choices: [
+            'MIT', 'Apache', 'ISC', 'Unlicense'
+        ],
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('README.md generated successfully.');
+        }
+      });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+      const markdown = generateMarkdown(answers);
+      writeToFile('README.md', markdown);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to create the README.md File
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ![Image Name ${data.license}](https://img.shields.io/badge/license-${data.license}-blue)
+
+  ## Description
+  ${data.description || ''}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation || ''}
+  
+  ## Usage
+  ${data.usage || ''}
+  
+  ## License
+  License under ${data.license}.
+  
+  ## Contributing
+  ${data.contributing || ''}
+  
+  ## Tests
+  ${data.tests || ''}
+  
+  ## Questions
+  For additional questions, contact me at [${data.email || 'your-email@email.com'}](mailto:${data.email || 'your-email@email.com'}).
+  Find me on GitHub: [${data.github || 'your-username'}](https://github.com/${data.github || 'your-username'})
+  `;
 }
 
 module.exports = generateMarkdown;
